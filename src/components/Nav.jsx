1753da7037eb1b6a257cef415eb7ebfd1907a01b
Oyerinde Daniel <dayniel247@gmail.com>
2025-07-13 12:41:40 +0100

    Sunday morning

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import Logo from "../assets/LOOOKL.png";
 import Linked from "./Linked";
+import "../pages/Responsive design/PhoneNav.css";
 import "./Nav.css";
 // import "../pages/Responsive design/PhoneNav.css";
 import "../pages/Responsive design/TabNav.css";
@@ -9,6 +10,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function Nav() {
   return (
     <>
+      <div className="PhoneNavDiv">
+        <nav className="phoneNav">
+          <ion-icon name="menu-outline" class="NavMenuBtn"></ion-icon>
+          <img src={Logo} id="nav_logo" />
+          <p id="logo">McDayniel's</p>
+        </nav>
+      </div>
+
       <nav id="nav_bar">
         <div id="nav_left">
           <img src={Logo} id="nav_logo" />
